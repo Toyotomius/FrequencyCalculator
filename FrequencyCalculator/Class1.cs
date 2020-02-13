57108bde7e2f81fea60d5b2e4524d177cc4b6ba3
Toyotomius <Toyotomius@gmail.com>
2020-02-13 18:59:35 -0400

    Add project files.

---
+++ FrequencyCalculator/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequencyCalculator
+{
+    public interface ICalculateFrequency<T>
+    {
+        public List<Singles<T>> CalculateSingles(IEnumerable<T> flatList);
+    }
+
+    public class Singles<T>
+    {
+        public int Frequency { get; set; }
+        public T Item { get; set; }
+    }
+
+    public class CalculateSingleFrequency<T> : ICalculateFrequency<T>
+    {
+       public List<Singles<T>> CalculateSingles(IEnumerable<T> flatList)
+        {
+            var query = (from item in flatList
+                         group item by item into g
+                         orderby g.Count() descending
+                         select new Singles<T>
+                         {
+                             Item = g.Key,
+                             Frequency = g.Count()
+                         }).ToList();
+            return query;
+        }
+    }
+}
---
+++ FrequencyConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using FrequencyCalculator;
+
+namespace FrequencyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testList = new List<string> { "2.2", "2.2", "3.1", "3.1", "1.2", "1.2", "1.2" };
+            ICalculateFrequency<string> freqCalc = new CalculateSingleFrequency<string>();
+            var result = freqCalc.CalculateSingles(testList);
+
+            foreach(var itm in result)
+            {
+                Console.WriteLine($"{itm.Item} Number : {itm.Frequency} Frequency");
+            }
+
+        }
+    }
+}
