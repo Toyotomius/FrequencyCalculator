37d5e9529c18d152c2217dd14d92f26092160bab
Toyotomius <Toyotomius@gmail.com>
2020-02-17 23:33:11 -0400

    Modified flatten extension method to be recursive.

--- FrequencyCalculator/IEnumerableExtensions/FlattenCollections.cs
+++ FrequencyCalculator/IEnumerableExtensions/FlattenCollections.cs
@@ -4,25 +4,8 @@ using System.Linq;
 
 namespace FrequencyCalculator
 {
-    public static class IEnumerableExtensions
+    public static class FlattenCollections
     {
-        /// <summary>
-        /// Checkes to see if IEnumerable contains a nested IEnumerable. Ignores string types.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="input"></param>
-        /// <returns>bool true if nested</returns>
-        public static bool IsNested<T>(this IEnumerable<T> input)
-        {
-            if(input.Any(x => x is null))
-            {
-                return false;
-            }
-            return (input.Any(x => x.GetType().GetInterfaces()
-             .Any(i => i.IsGenericType
-             && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
-             && input.Any(x => x.GetType() != typeof(string)));
-        }
         /// <summary>
         /// Flattens any nested enumerable passed. Ignores strings.
         /// </summary>
@@ -35,7 +18,7 @@ namespace FrequencyCalculator
                 IEnumerable candidate = element as IEnumerable;
                 if (candidate != null && candidate.GetType() != typeof(string))
                 {
-                    foreach (object nested in candidate)
+                    foreach (object nested in Flatten(candidate))
                     {
                         yield return nested;
                     }
@@ -46,5 +29,23 @@ namespace FrequencyCalculator
                 }
             }
         }
+
+        /// <summary>
+        /// Checkes to see if IEnumerable contains a nested IEnumerable. Ignores string types.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <returns>bool true if nested</returns>
+        //public static bool IsNested<T>(this IEnumerable<T> input)
+        //{
+        //    if (input.Any(x => x is null))
+        //    {
+        //        return false;
+        //    }
+        //    return (input.Any(x => x.GetType().GetInterfaces()
+        //     .Any(i => i.IsGenericType
+        //     && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+        //     && input.Any(x => x.GetType() != typeof(string)));
+        //}
     }
 }
