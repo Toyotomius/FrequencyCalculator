8a0038beb8f90a31479d5c309bad5266ffd8748e
Toyotomius <Toyotomius@gmail.com>
2020-03-13 20:53:03 -0300

    Added binary searching for triplets
    Added binary searching for Triplet code.
    
    Refactored triplet to code to better reduce repetition, utilizing the new helper methods.

---
+++ FrequencyCalculator/Helpers/TripletsCalculator/SpecificTripletsUnSorted.cs
@@ -0,0 +1,32 @@
+﻿using FrequencyCalculator.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequencyCalculator.Helpers
+{
+    internal static class SpecificTripletsUnSorted
+    {
+        /// <summary> Finds frequency of a distinct specific triplet of elements in an unsorted nested collection. </summary>
+        /// <typeparam name="T"> Type of base element </typeparam>
+        /// <param name="nestedCollection"> Collection of collections to iterate through to find distinct triplet frequency </param>
+        /// <param name="triplet">             triplet of elements to find the frequency of </param>
+        /// <returns> New triplet object of the triplet to find and their frequency </returns>
+        internal static Triplets<T> CalculateSpecifictripletsUnSorted<T>(IEnumerable<IList<T>> nestedCollection,
+                                                         IList<T> triplet) where T : IComparable<T>, IEquatable<T>
+        {
+            return (from n in nestedCollection
+                    where n != null
+                    where n.Contains(triplet[0]) && n.Contains(triplet[1]) && n.Contains(triplet[2])
+                    group n by triplet into g
+                    orderby g.Count() descending
+                    select new Triplets<T>
+                    {
+                        Item = triplet[0],
+                        Item2 = triplet[1],
+                        Item3 = triplet[2],
+                        Frequency = g.Count()
+                    }).First();
+        }
+    }
+}
--- FrequencyCalculator/IEnumerableExtensions/Triplets/TripletGroupSpecifiedFrequency.cs
+++ FrequencyCalculator/IEnumerableExtensions/Triplets/TripletGroupSpecifiedFrequency.cs
@@ -1,4 +1,5 @@
 ﻿using FrequencyCalculator.DataModels;
+using FrequencyCalculator.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,22 +8,23 @@ namespace FrequencyCalculator.IEnumerableExtensions
 {
     public static class TripletGroupSpecifiedFrequency
     {
-        /// <summary> 
-        /// Calculates frequency of a group of distinct tripets in nested collection using deferred execution.
-        /// For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
+        /// <summary>
+        /// Calculates frequency of a group of distinct tripets in nested collection using deferred execution. For
+        /// custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
         /// </summary>
         /// <typeparam name="T"> Type of base element in collections </typeparam>
         /// <param name="nestedCollection"> Nested collection to calculate triplet frequency from </param>
         /// <param name="tripletGroup">    
-        /// Collection of sub collections of distinct triplets to find the frequency of in the nested collection. 
-        /// Null in place of sub collection are ignored.
+        /// Collection of sub collections of distinct triplets to find the frequency of in the nested collection. Null
+        /// in place of sub collection are ignored.
         /// </param>
         /// <returns> IEnumerable of triplets objects in descending order of frequency </returns>
         /// <exception cref="ArgumentException">
         /// Thrown when a sub collection does not contain exactly three distinct values. Includes nulls in sub collection.
         /// </exception>
-        public static IEnumerable<Triplets<T>> CalculateTriplets<T>(this IEnumerable<IEnumerable<T>> nestedCollection,
-                                                                    IEnumerable<IList<T>> tripletGroup) where T : IComparable<T>, IEquatable<T>
+        public static IEnumerable<Triplets<T>> CalculateTriplets<T>(this IEnumerable<IList<T>> nestedCollection,
+                                                                    IEnumerable<IList<T>> tripletGroup,
+                                                                    bool IsSorted = false) where T : IComparable<T>, IEquatable<T>
         {
             var index = 0;
             foreach (var triplet in tripletGroup)
@@ -34,19 +36,13 @@ namespace FrequencyCalculator.IEnumerableExtensions
                 {
                     throw new ArgumentException($"Group {index} of {nameof(triplet)} in {nameof(tripletGroup)} does not contain exactly three distinct values");
                 }
+                if (IsSorted)
+                {
+                    var frequency = SpecificCollectionSorted.CalculateFrequencyOfGroup(nestedCollection, triplet);
+                    yield return new Triplets<T> { Item = triplet[0], Item2 = triplet[1], Item3 = triplet[2], Frequency = frequency };
+                }
+                else { yield return SpecificTripletsUnSorted.CalculateSpecifictripletsUnSorted(nestedCollection, triplet); }
 
-                yield return (from n in nestedCollection
-                              where n != null
-                              where n.Contains(triplet[0]) && n.Contains(triplet[1]) && n.Contains(triplet[2])
-                              group n by triplet into g
-                              orderby g.Count() descending
-                              select new Triplets<T>
-                              {
-                                  Item = triplet[0],
-                                  Item2 = triplet[1],
-                                  Item3 = triplet[2],
-                                  Frequency = g.Count()
-                              }).First();
                 index++;
             }
         }
