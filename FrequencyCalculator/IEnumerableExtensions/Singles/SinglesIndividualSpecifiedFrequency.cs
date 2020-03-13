4d5c5ca80b54c6cb6db2965d7d05505fb97c4091
Toyotomius <Toyotomius@gmail.com>
2020-03-13 18:41:26 -0300

    Corrected bug finding specified singles.
    Bug causing SortedSingles to be called on nested collections erroneously corrected.
    
    Removed IsSorted boolean requirement for nested elements as there is no guarantee that when collections are flattened they would remain sorted.
    
    Added helper methods to calculate requested singles (both individual and group) when collection is nested as part of the bug fix.
    
    Adjusted XML comments to reflect change.
    
    Added basic tests for helper methods.

--- FrequencyCalculator/IEnumerableExtensions/Singles/SinglesIndividualSpecifiedFrequency.cs
+++ FrequencyCalculator/IEnumerableExtensions/Singles/SinglesIndividualSpecifiedFrequency.cs
@@ -8,21 +8,52 @@ namespace FrequencyCalculator.IEnumerableExtensions
     public static class SinglesIndividualSpecifiedFrequency
     {
         /// <summary>
+        /// Calculates frequency of an individual item from collections. Will use binary search algorithm if IsSorted is
+        /// true. For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of collection elements to be calculated. Must implement IComparable and IEquatable for custom types
+        /// </typeparam>
+        /// <param name="collection"> Collection to be searched. Must not be nested </param>
+        /// <param name="value">      Value of the individual element you want the frequency of </param>
+        /// <param name="IsSorted">  
+        /// Default - false: Linear search for unsorted or nested collections.
+        /// True: Binary search for flat sorted collections (much faster). (Note: True does not check for nested
+        /// collections. If using a nested collection leave argument empty)
+        /// </param>
+        /// <returns> New Singles object with the value and number of occurrences </returns>
+        public static Singles<T> CalculateSingles<T>(this IList<T> collection, T value,
+                                                     bool IsSorted = false) where T : IComparable<T>, IEquatable<T>
+        {
+            if (IsSorted)
+            { return SinglesSorted.CalculateSinglesSorted(collection, value); }
+
+            var count = 0;
+
+            foreach (var itm in collection)
+            {
+                if (itm.Equals(value))
+                {
+                    count++;
+                }
+            }
+
+            return new Singles<T> { Item = value, Frequency = count };
+        }
+
+        /// <summary>
         /// Calculates frequency of an individual item from collections. Uses recursion to flatten nested collections.
         /// For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
         /// </summary>
-        /// <typeparam name="T"> Type of collection elements to be calculated. Must implement IComparable </typeparam>
-        /// <param name="collection"> Collection to be searched. Can be nested. </param>
-        /// <param name="value">      Value of the individual element you want the frequency of </param>
-        /// <param name="IsSorted">  
-        /// False: Linear search for unsorted or nested collections.
-        /// True: Binary search for flat sorted collections (much faster)
-        /// </param>
+        /// <typeparam name="T">
+        /// Type of collection elements to be calculated. Must implement IComparable and IEquatable for custom types
+        /// </typeparam>
+        /// <param name="collection"> Collection to be searched. Used for nested collections </param>
+        /// <param name="value">      value of the individual element you want the frequency of </param>
         /// <returns> New Singles object with the value and number of occurrences </returns>
-        public static Singles<T> CalculateSingles<T>(this IList<T> collection, T value, bool IsSorted) where T : IComparable<T>, IEquatable<T>
+        public static Singles<T> CalculateSingles<T>(this IEnumerable<IList<T>> collection, T value)
+                                                     where T : IComparable<T>, IEquatable<T>
         {
-            if (IsSorted) { return SinglesSorted.CalculateSinglesSorted(collection, value); }
-
             var flattened = collection.Flatten<T>();
 
             var count = 0;
--- FrequencyCalculator/IEnumerableExtensions/Singles/SinglesMultipleSpecifiedFrequency.cs
+++ FrequencyCalculator/IEnumerableExtensions/Singles/SinglesMultipleSpecifiedFrequency.cs
@@ -7,15 +7,15 @@ namespace FrequencyCalculator.IEnumerableExtensions
     public static class SinglesMultipleSpecifiedFrequency
     {
         /// <summary> 
-        /// Calculates frequency of an individual item from collections. Uses recursion to flatten nested collections. 
-        /// For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
+        /// Calculates frequency of an individual item from collections. Will use binary search algorithm if IsSorted is 
+        /// true. For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
         /// </summary>
         /// <typeparam name="T"> Type of collection elements to be calculated. Must implement IComparable </typeparam> 
         /// <param name="collection"> Collection to be searched. Can be nested. </param> 
         /// <param name="values"> Collection of values you want the frequency of. Ignores null elements </param> 
         /// <param name="IsSorted">
         /// False: Linear search for unsorted or nested collections.
-        /// True: Binary search for flat sorted collections (much faster) 
+        /// True: Binary search for flat sorted collections (much faster for large collections) 
         /// </param> 
         /// <returns> IEnumerable of Singles<T> object with the value and number of occurrences</returns>
         public static IEnumerable<Singles<T>> CalculateSingles<T>(this IList<T> collection, IList<T> values,
@@ -25,6 +25,25 @@ namespace FrequencyCalculator.IEnumerableExtensions
             {
                 if(value is null) { continue; }
                 yield return collection.CalculateSingles(value, IsSorted);
+            }
+        }
+        /// <summary>
+        /// Calculates frequency of an individual item from collections. Uses recursion to flatten nested collections.
+        /// For custom types ensure you override GetHashCode when implementing IEquatable for correct grouping.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Type of collection elements to be calculated. Must implement IComparable and IEquatable for custom types
+        /// </typeparam>
+        /// <param name="collection"> Collection to be searched. Used for nested collections </param>
+        /// <param name="values">      Collection of values you want the frequency of. Ignores null elements </param>
+        /// <returns> IEnumerable of Singles<T> object with the value and number of occurrences </returns>
+        public static IEnumerable<Singles<T>> CalculateSingles<T>(this IEnumerable<IList<T>> collection, IList<T> values)
+                                                                   where T : IComparable<T>, IEquatable<T>
+        {
+            foreach (var value in values)
+            {
+                if (value is null) { continue; }
+                yield return collection.CalculateSingles(value);
             }
         }
     }
